48d47484bc61ab0eb18e323695b128e92c494203
Designer-PC <chiragp.designer@gmail.com>
2021-05-03 16:25:57 +0530

    Adding more 2 pages with html.

---
+++ pages/legales.vue
@@ -0,0 +1,101 @@
+<template>
+    <main>
+        <div class="breadc-bak">
+            <div class="container margin-0-auto">
+                <b-breadcrumb class="ff-sans-r mb-2">
+                    <b-breadcrumb-item to="/">Home</b-breadcrumb-item>
+                    <b-breadcrumb-item active>Legales</b-breadcrumb-item>
+                </b-breadcrumb>
+            </div>
+        </div>
+        <section class="terms">
+            <div class="container margin-0-auto">
+                <div class="row">
+                    <div class="col-md-8 offset-md-2">
+                        <h1 class="text-center">Términos y condiciones</h1>
+                        <p>Portal - Fundación Bolivar Davivienda</p>
+                        <h3>Términos generales</h3>
+                        <ol class="columns">
+                            <li><a href="#no1">Información</a></li>
+                            <li><a href="#">Contenido y funcionalidades de El Portal</a></li>
+                            <li><a href="#">Uso de El Portal</a></li>
+                            <li><a href="#">Restricciones de uso de El Portal</a></li>
+                            <li><a href="#">Autorizaciones que usted otorga</a></li>
+                            <li><a href="#">Derecho de autor</a></li>
+                            <li><a href="#">Otros activos de propiedad industrial</a></li>
+                            <li><a href="#">Limitación de responsabilidad y garantía</a></li>
+                            <li><a href="#">Seguridad de la información</a></li>
+                            <li><a href="#">Incumplimiento e indemnización</a></li>
+                            <li><a href="#">Cambios en el contenido de El Portal</a></li>
+                        </ol>
+                        <hr />
+                        <p>Estos Términos y Condiciones regulan sus derechos y obligaciones respecto al uso del PORTAL FUNDACIÓN BOLÍVAR DAVIVIENDA (en adelante, “El Portal”) y de los servicios e información que se suministra a través de El Portal</p>
+                        <p>El uso que Usted haga del software, del contenido, de la información y de los servicios que proporcione El Portal se excluyen y no interferirán con otros servicios que GRUPO EMPRESARIAL BOLÍVAR, la FUNDACIÓN BOLÍVAR DAVIVIENDA y/o la FUNDACIÓN CULTIVA EL ARTE Y LA CULTURA pueda(n) proporcionarle en virtud de cualquier otro arreglo consensuado por escrito.</p>
+                        <p>Si Usted requiere información adicional o tiene alguna pregunta sobre el funcionamiento de El Portal, puede contactarnos a través de la dirección electrónica info@fundacionbd.org. Un miembro de FBD se encargará de dar respuesta.</p>
+                        <p>Al acceder a El Portal, navegar y/o usar los servicios ofrecidos en éste, Usted acepta haber leído, entendido y aceptado sin ningún tipo de reserva, y se está obligando conforme a estos Términos y Condiciones, la Política de Tratamiento de Datos Personales y la Política de Privacidad de la FUNDACIÓN BOLÍVAR DAVIVIENDA y la FUNDACIÓN CULTIVA EL ARTE Y LA CULTURA, constituyendo un acuerdo legal y vinculante. Los Términos Generales describen sus derechos y obligaciones en relación con la generalidad de El Portal y los distintos servicios, programas e información que, de manera general, se ofrecen dentro de El Portal (en adelante, los “Servicios, Programas e Información”). Por su parte, los Términos Específicos describen sus derechos y obligaciones respecto a secciones específicas de El Portal. El desconocimiento de su contenido no será causal de exoneración por su incumplimiento.</p>
+                        <hr />
+                        <h3 id="no1">Terminos generales</h3>
+                        <p><strong>1. Información</strong></p>
+                        <p>FUNDACIÓN BOLÍVAR DAVIVIENDA es una fundación constituida conforme a las leyes de Colombia, identificada con NIT. 860.076.173-2, con domicilio en la ciudad de Bogotá D.C., que para los efectos de los presentes términos se denominará “FBD”. Los términos “Usted”, “Usuario”, “su” o “sus”, se refiere en propiedad a la persona que acceda a El Portal y/o utilice sus Servicios, Programas e Información, con los cuales se entiende celebrado el acuerdo contenido en estos Términos y Condiciones de Uso de El Portal.</p>
+                        <p>Los Términos Generales aplican sobre la totalidad de El Portal, sin perjuicio que para algunas de sus secciones apliquen lo indicado en los Términos Específicos.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+</template>
+
+<style>
+.terms {
+    padding: 50px 0;
+}
+.terms h1 {
+    font-family: 'sansationbold';
+    font-size: 60px;
+    color: #2f425b;
+    text-decoration: none solid rgb(47, 66, 91);
+    line-height: 30px;
+    text-align: center;
+    margin-bottom: 30px;
+}
+.terms .columns {
+    columns: 2;
+    -webkit-columns: 2;
+    -moz-columns: 2;
+    font-family: 'sansationbold';
+    font-size: 16px;
+    color: #030303;
+    text-decoration: none solid rgb(3, 3, 3);
+    margin-bottom: 15px;
+}
+.terms .columns li {
+    margin: 10px 0;    
+}
+.terms .columns a {
+    color: #030303;
+    text-decoration: none;
+}
+.terms .columns a:hover {
+    color: #ff671b;
+}
+.terms h3 {
+    font-family: 'sansationbold';
+    font-size: 48px;
+    color: #2f425b;
+    text-decoration: none solid rgb(47, 66, 91);
+    line-height: 60px;
+    margin-bottom: 15px;
+}
+.terms p {
+    font-family: Nunito;
+    font-size: 18px;
+    color: #111820;
+    text-decoration: none solid rgb(17, 24, 32);
+    margin-bottom: 15px;
+}
+.terms hr {
+    margin: 30px 0;
+    border: none;
+    border-bottom: 3px solid #ff671b;
+}
+</style>
---
+++ pages/mapa-del-sitio.vue
@@ -0,0 +1,69 @@
+<template>
+    <main>
+        <section class="sitemap">
+            <div class="container margin-0-auto">
+                <div class="row">
+                    <div class="col-md-8 offset-md-2">
+                        <h1 class="text-center">Términos y condiciones</h1>
+                        <ul>
+                            <li><a href="#">Nombre de página</a>                            
+                                <ul>
+                                    <li><a href="#">Nombre de página</a></li>
+                                    <li><a href="#">Nombre de página</a></li>
+                                    <li><a href="#">Nombre de página</a>
+                                        <ul>
+                                            <li><a href="#">Nombre de página</a></li>
+                                            <li><a href="#">Nombre de página</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Gestión de procesos</a></li>
+                            <li><a href="#">Estrategia</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+</template>
+<style>
+.sitemap {
+    padding: 50px 0;
+}
+.sitemap h1 {
+    font-family: 'sansationbold';
+    font-size: 60px;
+    color: #2f425b;
+    text-decoration: none solid rgb(47, 66, 91);
+    line-height: 30px;
+    text-align: center;
+    margin-bottom: 30px;
+}
+.sitemap ul {
+    margin: 0;
+    margin-left: 15px;
+}
+.sitemap ul li {
+    position: relative;
+    padding: 5px 0 5px 15px;
+    font-family: Nunito;
+    font-size: 18px;
+    color: #2f425b;
+    text-decoration: none solid rgb(47, 66, 91);
+    line-height: 32px;
+}
+.sitemap ul li:before {
+    content: '';
+    position: absolute;
+    left: 0;
+    top: 20px;
+    width: 4px;
+    height: 4px;
+    border-radius: 50%;
+    background: #2f425b;
+}
+.sitemap ul a:hover {
+    color: #ff671b;
+}
+</style>
