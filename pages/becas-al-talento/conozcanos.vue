139e843a146e50f3e486b364e12989f72555433f
Saumil Choksi <saumilchoksi.ce05@gmail.com>
2021-05-03 21:05:18 +0530

    Integrate Becas Home page and Conozcanos page.

---
+++ pages/becas-al-talento/conozcanos.vue
@@ -0,0 +1,219 @@
+<template>
+  <main>
+    <!-- Banner start -->
+    <section class="common-banner">
+      <div class="container">
+        <h2>Becas al talento</h2>
+      </div>
+    </section>
+    <!-- Banner end -->
+
+    <!-- image text left right start -->
+    <section class="txt-image left-txt space-big">
+      <div class="container">
+        <div class="txt-img-multi">
+          <div class="only-txt">
+            <div class="single-txt">
+              <h2>Sobre Becas</h2>
+              <p>En el programa apoyamos el talento y la excelencia de los estudiantes de nuestras universidades aliadas, con el fin de garantizar que terminen sus estudios profesionales de manera exitosa. Este beneficio se otorga como una beca-crédito para estudiantes que estén cursando entre 5to y 8vo semestre y está ligado a las políticas y procesos internos de las instituciones universitarias.</p>
+            </div>
+          </div>
+          <div class="only-img">
+            <img src="/images/nuestro.jpg" alt="">
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- image text left right end -->
+
+    <!-- Hoy el start -->
+    <section class="hoy-main">
+      <div class="container">
+        <div class="hoy-img-txt">
+          <div class="hoy-img">
+            <img src="/images/hoy.jpg" alt="">
+          </div>
+          <div class="hoy-txt">
+            <div class="h-txt-main">
+              <p>Hoy el programa ha beneficiado a más de 300 estudiantes, y espera seguir creciendo</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Hoy el end -->
+
+    <!-- Nuestros start -->
+    <section class="nuestro-main">
+      <div class="container">
+        <div class="nuestro-title">
+          <h3>Nuestros</h3>
+          <h2>objetivos son</h2>
+        </div>
+        <div class="nuestro-box">
+          <div class="nuestro-single">
+            <h4>Con las universidades aliadas</h4>
+            <img src="/images/universidades.jpg" alt="">
+            <p>Constituir el fondo de becas crédito en alianza con cada institución educativa. Con los recaudos de capital que vaya gestionando este fondo, el programa seguirá entregando estos beneficios financieros a otros estudiantes que cumplan con el perfil definido.</p>
+          </div>
+          <div class="nuestro-single">
+            <h4>Con los estudiantes y sus familias</h4>
+            <img src="/images/estudiantes.jpg" alt="">
+            <p>Ofrecer alternativas de financiación para que los estudiantes con alto desempeño académico logren culminar sus estudios de pregrado sin inconvenientes.</p>
+          </div>
+          <div class="nuestro-single">
+            <h4>Con la sociedad colombiana</h4>
+            <img src="/images/sociedad.jpg" alt="">
+            <p>Formar profesionales íntegros, con alto sentido de responsabilidad social, jóvenes líderes comprometidos con la construcción de una sociedad más justa, incluyente e innovadora.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Nuestros end -->
+
+    <!-- Testimonial start -->
+    <section class="testimonial">
+      <div class="testi-title">
+        <h3>Testimonios</h3>
+        <h4>De nuestros integrantes</h4>
+      </div>
+      <div class="testi-center">
+        <div class="testi-single">
+          <div class="testi-txt">
+            <h3>Sara Castro - 10</h3>
+            <p>La educación financiera es como las matemáticas, solo que aquí juegas con los números y aprendes a ahorrar jugando como si fueras un banco de verdad</p>
+          </div>
+        </div>
+        <div class="testi-single">
+          <div class="testi-txt">
+            <h3>Dilan Cabezas - 9</h3>
+            <p>Los procesos de lectura, como ‘viajes de papel’, son la forma en la que puedes vivir el libro con juegos, sentirlo y que lo puedas recordar siempre</p>
+          </div>
+        </div>
+        <div class="testi-single">
+          <div class="testi-txt">
+            <h3>Mariangel Molina - 7</h3>
+            <p>Lo que más me gusta de Cultivarte es el Coro porque me fascina cantar y quiero ser una cantante famosa cuando sea grande</p>
+          </div>
+        </div>
+        <div class="testi-single">
+          <div class="testi-txt">
+            <h3>Sara Castro - 10</h3>
+            <p>La educación financiera es como las matemáticas, solo que aquí juegas con los números y aprendes a ahorrar jugando como si fueras un banco de verdad</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Testimonial end -->
+
+    <!-- image text left right start -->
+    <section class="txt-image left-txt space-big">
+      <div class="container">
+        <div class="txt-img-multi">
+          <div class="only-txt">
+            <div class="single-txt">
+              <p class="pb-4">En Becas al Talento estamos comprometidos con fortalecer las habilidades blandas y <b>Competencias Ciudadanas</b> de todos nuestros beneficiarios, con el propósito de convertir sus talentos en oportunidades de mejora.</p>
+              <p>Deseamos apoyar el proceso académico y personal de nuestros estudiantes, siempre desde el respeto y la disciplina, para cumplir metas y celebrar triunfos junto a ellos.</p>
+            </div>
+          </div>
+          <div class="only-img">
+            <img src="/images/img2.jpg" alt="">
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- image text left right end -->
+
+    <!-- image text left right start -->
+    <section class="txt-image right-txt right-top">
+      <div class="container">
+        <div class="txt-img-multi">
+          <div class="only-img">
+            <img src="/images/img3.jpg" alt="">
+          </div>
+          <div class="only-txt">
+            <div class="single-txt">
+              <p class="pb-4">Creemos que las habilidades blandas son las bases del éxito, por eso hemos creado 3 ejes principales para nuestro curso de competencias ciudadanas:</p>
+              <ul>
+                <li>Habilidades blandas: personales y sociales.</li>
+                <li>Competencias integradoras.</li>
+                <li>Acciones transformadoras.</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- image text left right end -->
+
+    <!-- Con el apoyo de start -->
+    <section class="conel">
+      <div class="container">
+        <div class="con-el">
+          <h3>Con el apoyo de:</h3>
+          <div class="services">
+            <div class="single-service">
+              <p>Pontificia Universidad Javeriana</p>
+            </div>
+            <div class="single-service">
+              <p>Pontificia Universidad Javeriana</p>
+            </div>
+            <div class="single-service">
+              <p>Pontificia Universidad Javeriana</p>
+            </div>
+            <div class="single-service">
+              <p>Pontificia Universidad Javeriana</p>
+            </div>
+            <div class="single-service">
+              <p>Pontificia Universidad Javeriana</p>
+            </div>
+            <div class="single-service">
+              <p>Pontificia Universidad Javeriana</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+<script>
+export default {
+  methods: {
+    goBack() {
+      window.history.back()
+    },
+  },
+  head() {
+    return {
+      title: this.$constants.afloraConozcanos.meta.title,
+      meta: [
+        {
+          hid: 'title',
+          name: 'title',
+          content: this.$constants.afloraConozcanos.meta.metaTitle,
+        },
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.$constants.afloraConozcanos.meta.metaDescription,
+        },
+        {
+          hid: 'og:title',
+          name: 'og:title',
+          content: this.$constants.afloraConozcanos.meta.ogTitle,
+        },
+        {
+          hid: 'og:description',
+          name: 'og:description',
+          content: this.$constants.afloraConozcanos.meta.ogDescription,
+        },
+        {
+          hid: 'og:image',
+          name: 'og:image',
+          content: this.$constants.baseURL + this.$constants.afloraConozcanos.meta.ogImage,
+        },
+      ],
+    }
+  },
+}
+</script>
---
+++ pages/becas-al-talento/index.vue
@@ -0,0 +1,102 @@
+<template>
+  <main>
+    <section class="becas-banner">
+      <div class="container-fluid">
+        <div class="banner-content">
+          <div class="b-logo">
+            <img src="/images/becas-banner-logo.png" alt="">
+          </div>
+          <p>Becas al Talento es un programa de la Fundación Bolívar Davivienda, que busca apoyar a estudiantes con un alto desempeño académico, de diferentes instituciones de educación superior para culminar sus estudios en caso de dificultades económicas. º</p>
+        </div>
+      </div>
+      <div class="banner-img">
+        <img src="/images/becas-banner-mobile.jpg" alt="">
+      </div>
+    </section>
+    <!-- Banner end -->
+
+    <!-- Link start -->
+    <section class="becas-links">
+      <div class="container">
+        <div class="link-main">
+          <NuxtLink to="/becas-al-talento/conozcanos" class="single-link"
+            >Conózcanos</NuxtLink
+          >
+          <NuxtLink to="/becas-al-talento/conozcanos" class="single-link"
+            >Desarrollamos talento</NuxtLink
+          >
+          <NuxtLink to="/becas-al-talento/conozcanos" class="single-link"
+            >Requisitos</NuxtLink
+          >
+        </div>
+      </div>
+    </section>
+    <!-- Link end -->
+
+    <!-- Sobre Becas start -->
+    <section class="sobre-becas txt-image right-txt">
+      <div class="container">
+        <div class="sobre-main txt-img-multi">
+          <div class="sobre-img only-img">
+            <img src="/images/sobre.jpg" alt="">
+          </div>
+          <div class="sobre-content only-txt">
+            <div>
+              
+            </div>
+            <div class="sobre-txt single-txt">
+              <h2>Sobre Becas</h2>
+              <p>En el programa apoyamos el talento y la excelencia de los estudiantes de nuestras universidades aliadas, con el fin de garantizar que terminen sus estudios profesionales de manera exitosa. Este beneficio se otorga como una beca-crédito para estudiantes que estén cursando entre 5to y 8vo semestre y está ligado a las políticas y procesos internos de las instituciones universitarias.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Sobre Becas end -->
+
+    <!-- call to action start -->
+    <section class="cta">
+      <div class="container">
+        <h3>Sobre nosotros</h3>
+        <h2>Desarrollamos Talento</h2>
+        <a :href="$constants.becasAlTalento.cuentameMasLink">Cuéntame más</a>
+      </div>
+    </section>
+  </main>
+</template>
+<script>
+export default {
+  head() {
+    return {
+      title: this.$constants.becasAlTalento.meta.title,
+      meta: [
+        {
+          hid: 'title',
+          name: 'title',
+          content: this.$constants.becasAlTalento.meta.metaTitle,
+        },
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.$constants.becasAlTalento.meta.metaDescription,
+        },
+        {
+          hid: 'og:title',
+          name: 'og:title',
+          content: this.$constants.becasAlTalento.meta.ogTitle,
+        },
+        {
+          hid: 'og:description',
+          name: 'og:description',
+          content: this.$constants.becasAlTalento.meta.ogDescription,
+        },
+        {
+          hid: 'og:image',
+          name: 'og:image',
+          content: this.$constants.baseURL + this.$constants.becasAlTalento.meta.ogImage,
+        },
+      ],
+    }
+  },
+}
+</script>
