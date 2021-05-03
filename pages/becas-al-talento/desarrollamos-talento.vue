88cbe0eade3ab8614eac772012c1d674d3fd4885
dustin-shelton <dustin.shelton@ftxinfotech.com>
2021-05-04 02:18:06 +0530

    adding becas-al-talento/desarrollamos-talento

---
+++ pages/becas-al-talento/desarrollamos-talento.vue
@@ -0,0 +1,85 @@
+<template>
+  <main>
+    <!-- Breadcrums start -->
+    <div class="breadc-bak">
+      <div class="container margin-0-auto">
+        <ol class="breadcrumb ff-sans-r mb-2">
+          <li class="breadcrumb-item">
+            <a href="/." target="_self" class="">Home</a>
+          </li>
+          <li class="breadcrumb-item active">
+            <span aria-current="location">Desarrollamos talento</span>
+          </li>
+        </ol>
+        <div
+          class="back-to-home position-relative d-flex align-items-center small-size"
+        >
+          <i class="ic-back"></i>
+          <a href="javascript:void(0)" class="ff-sans-b">Volver</a>
+        </div>
+      </div>
+    </div>
+    <!-- Breadcrums end -->
+
+    <!-- image text left right start -->
+    <section class="txt-image left-txt space-big">
+      <div class="container">
+        <div class="txt-img-multi">
+          <div class="only-txt outer-txt">
+            <div class="b-title">
+              <h2>Desarrollamos talento</h2>
+            </div>
+            <div class="single-txt">
+              <p>
+                Desarrollamos en nuestros beneficiarios la capacidad de resolver
+                inconvenientes, encontrando nuevos y mejores caminos que los
+                lleven a cumplir sus objetivos y metas.
+              </p>
+            </div>
+            <div>
+              <img src="/images/oval-1.png" alt="" />
+            </div>
+          </div>
+          <div class="only-img">
+            <img src="/images/nuestro.jpg" alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- image text left right end -->
+
+    <!-- image text left right start -->
+    <section class="txt-image right-txt right-top pt-0">
+      <div class="container">
+        <div class="txt-img-multi">
+          <div class="only-img">
+            <img src="/images/lady.jpg" alt="" />
+          </div>
+          <div class="only-txt">
+            <div class="single-txt top-small">
+              <p class="pb-4">
+                En Becas al Talento hemos creado una serie de talleres que
+                fortalecerán el talento de estos jóvenes:
+              </p>
+              <ul>
+                <li>Liderazgo transformador</li>
+                <li>
+                  Práctica de <i><b>mindfulness</b></i>
+                </li>
+                <li>Imagen profesional</li>
+                <li>Negociación y trabajo en equipo</li>
+                <li>Presentaciones de alto impacto</li>
+                <li>Sustentación – Acto comunicativo</li>
+                <li>Metodologías de proyectos</li>
+                <li>Innovación</li>
+                <li>Conocimiento digital</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- image text left right end -->
+  </main>
+</template>
+
