1ad9911df2af765fcded0b7018df0891b9ace84e
Designer-PC <chiragp.designer@gmail.com>
2021-05-04 15:30:31 +0530

    Formatting Pending Pages.

---
+++ pages/red-de-angeles.vue
@@ -0,0 +1,356 @@
+<template>
+    <main>
+        <section class="invester-section-1">
+            <div class="container margin-0-auto">                
+                <div class="row">
+                    <div class="col-md-12 text-center leader-title">
+                        <h1 class="ff-sans-b">Red de Ángeles inversionistas</h1>
+                        <p class="text-orange">Un espacio donde su liderazgo tendrá impacto nacional</p>                        
+                    </div>
+                    <div class="col-md-12">
+                        <div class="sec1-boxes">
+                            <b-img src="images/invester-about.jpg" fluid alt=""></b-img>
+                            <div class="sec1-description">
+                                <h3 class="ff-sans-b">¿Quiénes Somos?</h3>
+                                <p>La Red de Ángeles Inversionistas es un programa que apuesta por la transformación social de negocios rentables y sostenibles. Esta Red busca generar un cambio en la narrativa de la inversión de riesgo y de impacto para crecer como sociedad. Así mismo, creamos espacios y conexiones de valor para apoyar a los empresarios locales por medio de herramientas y metodologías que les permitan fortalecer sus capacidades para el levantamiento de capital e inversión con impacto.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="invester-section-2 bg-grey">
+            <div class="container margin-0-auto">
+                <h2 class="ff-sans-b">Perfiles</h2>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="sec2-boxes">
+                            <div class="sec2-inner-box">
+                                <h4 class="ff-sans-b">Empresas</h4>
+                                <b-img src="images/invester-profile-1.jpg" fluid alt=""></b-img>
+                                <p>Una vez cursados los seis módulos del plan de estudios, los miembros de la cohorte aplican el conocimiento y las habilidades adquiridas, para escribir y presentar un Reto de Emprendimiento. Esta evaluación final puede ser muy versátil, dependiendo de los intereses, creatividad y antecedentes de cada participante.</p>
+                                <div class="text-center">
+                                    <b-button variant="primary">Inscribase aquí</b-button>
+                                </div>
+                            </div>
+                            <div class="sec2-inner-box">
+                                <h4 class="ff-sans-b">Inversionista</h4>
+                                <b-img src="images/invester-profile-2.jpg" fluid alt=""></b-img>
+                                <p>Los inversionistas que participan en la Red de Ángeles Inversionistas han impactado al país con su liderazgo y extensa experiencia dentro de su industria. Son, además, profesionales que cuentan con un espíritu emprendedor innato, y tienen una capacidad de inversión entre el 5% y el 20% de su patrimonio, con la cual le apostarán a start-ups o en empresas en una etapa temprana.</p>
+                                <div class="text-center">
+                                    <b-button variant="primary">Inscribase aquí</b-button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="invester-section-3">
+            <div class="container margin-0-auto">                
+                <div class="row">
+                    <div class="col-md-8 offset-md-2">
+                        <h2 class="ff-sans-b">Requisitos para aplicar a la red como emprendendor</h2>
+                        <div class="sec3-boxes">
+                            <div class="row align-items-center">
+                                <div class="col-md-4">
+                                    <b-img src="images/icon-user-group.png" fluid alt=""></b-img>                                    
+                                </div>
+                                <div class="col-md-8">
+                                    <p>Su emprendimiento no destruye valor social o ambiental</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="invester-section-4 bg-grey testimonios-sec">
+            <div class="container-fluid">
+                <div class="sec-title text-center">
+                    <h2 class="ff-sans-b">Testimonios</h2>
+                </div>
+                <!-- <div class="testimo-slider">
+                <div class="for-marg-1">
+                    <client-only>
+                    <carousel
+                        :perPageCustom="[
+                        [575, 1],
+                        [1024, 2],
+                        [1200, 3],
+                        ]"
+                        :navigationEnabled="true"
+                        :paginationEnabled="true"
+                    >
+                        <slide
+                        v-for="(item, i) of $constants.conozcanos.testimonios"
+                        :key="i"
+                        >
+                        <div class="position-relative testimo-bx bg-white">
+                            <div class="text-center til ff-sans-b">
+                            <h3>
+                                {{ item.title }}
+                            </h3>
+                            </div>
+                            <div
+                            class="txt margin-0-auto position-relative after-po before-po quote-sign ff-sans-i"
+                            >
+                            <p>
+                                {{ item.description }}
+                            </p>
+                            </div>
+                        </div>
+                        </slide>
+                    </carousel>
+                    </client-only>
+                </div>
+                </div> -->
+            </div>
+            </section>
+        <section class="invester-section-5">
+            <div class="container margin-0-auto">
+                <h4 class="ff-sans-b">Con el apoyo de:</h4>
+                <div class="d-flex flex-wrap for-marg-1">
+                <div class="lgoes-bx d-flex align-items-center position-relative">
+                    <img src="/images/gell_logo.svg" alt="" />
+                    <a
+                    href="javascript:void(0)"
+                    class="position-absolute full-box-link"
+                    ></a>
+                </div>
+                <div class="lgoes-bx d-flex align-items-center position-relative">
+                    <img src="/images/gell_logo.svg" alt="" />
+                    <a
+                    href="javascript:void(0)"
+                    class="position-absolute full-box-link"
+                    ></a>
+                </div>
+                <div class="lgoes-bx d-flex align-items-center position-relative">
+                    <img src="/images/gell_logo.svg" alt="" />
+                    <a
+                    href="javascript:void(0)"
+                    class="position-absolute full-box-link"
+                    ></a>
+                </div>
+                <div class="lgoes-bx d-flex align-items-center position-relative">
+                    <img src="/images/gell_logo.svg" alt="" />
+                    <a
+                    href="javascript:void(0)"
+                    class="position-absolute full-box-link"
+                    ></a>
+                </div>
+                <div class="lgoes-bx d-flex align-items-center position-relative">
+                    <img src="/images/gell_logo.svg" alt="" />
+                    <a
+                    href="javascript:void(0)"
+                    class="position-absolute full-box-link"
+                    ></a>
+                </div>
+                <div class="lgoes-bx d-flex align-items-center position-relative">
+                    <img src="/images/gell_logo.svg" alt="" />
+                    <a
+                    href="javascript:void(0)"
+                    class="position-absolute full-box-link"
+                    ></a>
+                </div>
+                <div class="lgoes-bx d-flex align-items-center position-relative">
+                    <img src="/images/gell_logo.svg" alt="" />
+                    <a
+                    href="javascript:void(0)"
+                    class="position-absolute full-box-link"
+                    ></a>
+                </div>
+                </div>
+            </div>
+        </section>
+    </main>
+</template>
+
+<style>
+.bg-grey {
+    background: #f7f7f7;
+}
+.text-orange {
+    color: #ff671b;
+}
+.invester-section-1 {
+    padding: 50px 0;
+}
+.invester-section-1 h1 {
+    font-size: 60px;
+    color: #2f425b;
+    text-decoration: none solid rgb(47, 66, 91);
+    text-align: center;
+}
+.invester-section-1 .text-orange {
+    font-family: Nunito;
+    font-size: 32px;
+    color: #ff671b;
+    text-decoration: none solid rgb(255, 103, 27);
+    line-height: 34px;
+    letter-spacing: 2px;
+    text-align: center;
+}
+.sec1-boxes {
+    margin-top: 30px;
+    position: relative;
+    overflow: hidden;
+}
+.sec1-boxes .img-fluid {
+    position: absolute;
+    z-index: 0;
+    right: 0;
+    top: 0;
+}
+.sec1-boxes .sec1-description {
+    position: relative;
+    z-index: 1;
+    padding: 50px;
+    width: 60%;
+    background: -moz-linear-gradient(left,  rgba(218,218,218,1) 0%, rgba(218,218,218,1) 50%, rgba(255,255,255,1) 75%, rgba(255,255,255,0) 100%);
+    background: -webkit-linear-gradient(left,  rgba(218,218,218,1) 0%,rgba(218,218,218,1) 50%,rgba(255,218,255,1) 75%, rgba(255,255,255,0) 100%);
+    background: linear-gradient(to right,  rgba(218,218,218,1) 0%,rgba(218,218,218,1) 50%,rgba(255,255,255,1) 75%, rgba(255,255,255,0) 100%);
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#dadada', endColorstr='#00dadada',GradientType=1 );
+}
+.sec1-boxes h3 {
+    font-size: 60px;
+    color: #2f425b;
+    text-decoration: none solid rgb(47, 66, 91);
+    line-height: 64px;
+    margin-bottom: 25px;
+}
+.sec1-boxes p {
+    font-family: Nunito;
+    font-size: 18px;
+    color: #313338;
+    text-decoration: none solid rgb(49, 51, 56);
+    line-height: 32px;
+}
+
+.invester-section-2 {
+    padding: 50px 0;
+}
+.invester-section-2 h2 {
+    font-size: 60px;
+    color: #2f425b;
+    text-decoration: none solid rgb(47, 66, 91);
+    text-align: center;
+}
+.sec2-boxes {
+    display: flex;
+    flex-direction: row;
+    align-items: stretch;
+    justify-content: center;
+    margin-top: 15px;
+}
+.sec2-boxes .sec2-inner-box {
+    background: #fff;
+    margin: 0 30px;
+    padding: 25px;
+    height: 100%;
+    flex: 0 0 35%;
+}
+.sec2-inner-box h4 {
+    font-size: 24px;
+    color: #2f425b;
+    text-decoration: none solid rgb(47, 66, 91);
+    line-height: 32px;
+    margin-bottom: 15px;
+}
+.sec2-inner-box p {
+    font-family: Nunito;
+    font-size: 18px;
+    color: #77777a;
+    text-decoration: none solid rgb(119, 119, 122);
+    line-height: 32px;
+    margin-bottom: 15px;
+}
+.sec2-inner-box .img-fluid {
+    margin-bottom: 15px;
+}
+.sec2-inner-box .btn-primary {
+    padding: 10px 25px;
+    background-color: #ff671b;
+    font-family: Nunito;
+    font-size: 18px;
+    color: #ffffff;
+    text-decoration: none solid rgb(255, 255, 255);
+    text-align: center;
+    border: none;
+    border-radius: 0;
+    margin-bottom: 15px;
+}
+
+.invester-section-3 {
+    padding: 50px 0;
+}
+.invester-section-3 h2 {
+    font-size: 60px;
+    color: #2f425b;
+    text-decoration: none solid rgb(47, 66, 91);
+    margin-bottom: 50px;
+    text-align: center;
+    position: relative;
+}
+.invester-section-3 h2:after {
+    content: '';
+    position: absolute;
+    width: 300px;
+    height: 1px;
+    background: #2f425b;
+    margin-left: -150px;
+    left: 50%;
+    bottom: -20px;
+}
+
+.sec3-boxes {
+    padding: 30px;
+    border-radius: 10px;
+    background: #f7f7f7;
+    box-shadow: 0px 22px 30px rgba(17,24,32,0.12);
+}
+.sec3-boxes {
+    text-align: center;
+}
+.sec3-boxes p {
+    font-size: 22px;
+    color: #111820;
+    text-decoration: none solid rgb(17, 24, 32);
+    line-height: 28px;
+}
+
+.invester-section-4 {
+    padding: 50px 0;
+}
+
+.invester-section-4 h2 {
+    font-size: 60px;
+    color: #2f425b;
+    text-decoration: none solid rgb(47, 66, 91);
+    text-align: center;
+    margin-bottom: 15px;
+}
+
+.invester-section-5 {
+    padding: 50px 0;
+}
+.invester-section-5 h4 {
+    color: #111820;
+    font-size: 20px;
+    margin: 0 0 15px;
+}
+.invester-section-5 .for-marg-1 {
+    margin: 0 -1%;
+}
+.invester-section-5 .lgoes-bx {
+    width: 12.28571%;
+    margin: 10px 1%;
+    height: 60px;
+}
+.invester-section-5 .lgoes-bx img {
+    max-width: 100%;
+    display: block;
+    margin: 0 auto;
+    max-height: 100%;
+}
+</style>
