99b0e3fe483363ff135d86954a4328b6902cf8e1
dustin-shelton <dustin.shelton@ftxinfotech.com>
2021-05-04 15:50:51 +0530

    adding 2-3-4 pages

---
+++ pages/cultivarte/donde-estamos-ubicados.vue
@@ -0,0 +1,332 @@
+<template>
+  <main>
+    <div class="donde-esta">
+      <!-- Breadcrums start -->
+      <div class="breadc-bak">
+        <div class="container margin-0-auto">
+          <ol class="breadcrumb ff-sans-r mb-2">
+            <li class="breadcrumb-item">
+              <a href="/." target="_self" class="text-dark">Home</a>
+            </li>
+            <li class="breadcrumb-item active">
+              <span aria-current="location">Requisitos</span>
+            </li>
+          </ol>
+          <div
+            class="back-to-home position-relative d-flex align-items-center small-size"
+          >
+            <i class="ic-back"></i>
+            <a href="javascript:void(0)" class="ff-sans-b text-white">Volver</a>
+          </div>
+        </div>
+      </div>
+      <!-- Breadcrums end -->
+
+      <!-- Banner start -->
+      <section class="common-banner ndb">
+        <div class="container">
+          <h2>¿Dónde estamos?</h2>
+        </div>
+      </section>
+      <!-- Banner end -->
+    </div>
+
+    <section class="page-details">
+      <div class="container">
+        <p>
+          Nuestro objetivo es llegar a lugares donde las ayudas y los programas
+          pedagógicos tienen mayores dificultades de consolidarse y de esta
+          manera convertirnos en un modelo de cambio para construir país.
+          Actualmente tenemos más de 80 sedes donde niños, niñas y adolescentes
+          encuentran espacios de ocio en los que desarrollan las competencias
+          necesarias para cumplir sus sueños.
+        </p>
+      </div>
+    </section>
+
+    <section class="all-tab">
+      <div class="container">
+        <ul class="nav nav-tabs" role="tablist">
+          <li class="nav-item">
+            <a class="nav-link active" data-toggle="tab" href="#colombia"
+              >Colombia</a
+            >
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" href="#el-salvador"
+              >El Salvador</a
+            >
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" href="#costa-rica"
+              >Costa Rica</a
+            >
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" href="#honduras">Honduras</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" href="#panamá">Panamá</a>
+          </li>
+        </ul>
+        <div class="tab-content">
+          <div id="colombia" class="container tab-pane active">
+            <div class="tab-main">
+              <div class="tab-drop">
+                <div class="drop-sub">
+                  <h2>Filtros</h2>
+                  <h3>Municipio</h3>
+                  <select>
+                    <option>Bogotá</option>
+                    <option>Bogotá</option>
+                    <option>Bogotá</option>
+                    <option>Bogotá</option>
+                    <option>Bogotá</option>
+                  </select>
+                </div>
+              </div>
+              <div class="contact-details">
+                <h2>CUNDINAMARCA</h2>
+                <h3>Bogotá</h3>
+                <h2>Alcaldía de Pacho</h2>
+                <h2>Coordinador</h2>
+                <a href="milto:bosa@fcultivarte.org">
+                  <img src="/images/email.svg" alt="" /> bosa@fcultivarte.org
+                </a>
+              </div>
+            </div>
+          </div>
+          <div id="el-salvador" class="container tab-pane fade">
+            <div class="tab-main">
+              <div class="tab-drop">
+                <div class="drop-sub">
+                  <h2>Filtros2</h2>
+                  <h3>Municipio</h3>
+                  <select>
+                    <option>Bogotá</option>
+                    <option>Bogotá</option>
+                    <option>Bogotá</option>
+                    <option>Bogotá</option>
+                    <option>Bogotá</option>
+                  </select>
+                </div>
+              </div>
+              <div class="contact-details">
+                <h2>CUNDINAMARCA</h2>
+                <h3>Bogotá</h3>
+                <h2>Alcaldía de Pacho</h2>
+                <h2>Coordinador</h2>
+                <a href="milto:bosa@fcultivarte.org"
+                  ><img src="/images/email.svg" alt="" />bosa@fcultivarte.org</a
+                >
+              </div>
+            </div>
+          </div>
+          <div id="costa-rica" class="container tab-pane fade">
+            <div class="tab-main">
+              <div class="tab-drop">
+                <div class="drop-sub">
+                  <h2>Filtros3</h2>
+                  <h3>Municipio</h3>
+                  <select>
+                    <option>Bogotá</option>
+                    <option>Bogotá</option>
+                    <option>Bogotá</option>
+                    <option>Bogotá</option>
+                    <option>Bogotá</option>
+                  </select>
+                </div>
+              </div>
+              <div class="contact-details">
+                <h2>CUNDINAMARCA</h2>
+                <h3>Bogotá</h3>
+                <h2>Alcaldía de Pacho</h2>
+                <h2>Coordinador</h2>
+                <a href="milto:bosa@fcultivarte.org"
+                  ><img src="/images/email.svg" alt="" />bosa@fcultivarte.org</a
+                >
+              </div>
+            </div>
+          </div>
+          <div id="honduras" class="container tab-pane fade">
+            <div class="tab-main">
+              <div class="tab-drop">
+                <div class="drop-sub">
+                  <h2>Filtros4</h2>
+                  <h3>Municipio</h3>
+                  <select>
+                    <option>Bogotá</option>
+                    <option>Bogotá</option>
+                    <option>Bogotá</option>
+                    <option>Bogotá</option>
+                    <option>Bogotá</option>
+                  </select>
+                </div>
+              </div>
+              <div class="contact-details">
+                <h2>CUNDINAMARCA</h2>
+                <h3>Bogotá</h3>
+                <h2>Alcaldía de Pacho</h2>
+                <h2>Coordinador</h2>
+                <a href="milto:bosa@fcultivarte.org"
+                  ><img src="/images/email.svg" alt="" />bosa@fcultivarte.org</a
+                >
+              </div>
+            </div>
+          </div>
+          <div id="panamá" class="container tab-pane fade">
+            <div class="tab-main">
+              <div class="tab-drop">
+                <div class="drop-sub">
+                  <h2>Filtros5</h2>
+                  <h3>Municipio</h3>
+                  <select>
+                    <option>Bogotá</option>
+                    <option>Bogotá</option>
+                    <option>Bogotá</option>
+                    <option>Bogotá</option>
+                    <option>Bogotá</option>
+                  </select>
+                </div>
+              </div>
+              <div class="contact-details">
+                <h2>CUNDINAMARCA</h2>
+                <h3>Bogotá</h3>
+                <h2>Alcaldía de Pacho</h2>
+                <h2>Coordinador</h2>
+                <a href="milto:bosa@fcultivarte.org">
+                  <img src="/images/email.svg" alt="" />bosa@fcultivarte.org
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+<style >
+.common-banner.ndb {
+	background-image: linear-gradient(0deg, #111820 12%, rgba(255, 103, 27, 0.1) 100% ), url(/images/header-desktop.jpg);
+}
+.donde-esta {
+	position: relative;
+}
+.donde-esta .breadc-bak {
+	position: absolute;
+	top: 0;
+	width: 100%;
+}
+.page-details {
+	padding: 40px 0;
+}
+.page-details p {
+	max-width: 693px;
+	margin: 0 auto;
+	font-family: sansationregular;
+	font-size: 18px;
+	color: #111820;
+	line-height: 30px;
+	text-align: center;
+}
+.all-tab ul {
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: center;
+	border: none;
+}
+.all-tab ul li {
+	width: 161px;
+	margin: 7px;
+}
+.all-tab ul li a.nav-link {
+	border: 1px solid #77777a;
+	border-radius: 4px;
+	background-size: cover;
+	font-family: Nunito,sans-serif;
+	font-size: 16px;
+	color: #77777a;
+	text-align: center;
+	padding: 10px;
+	display: block;
+}
+.all-tab ul li a.nav-link.active,
+.all-tab ul li a.nav-link:hover {
+	background-color: #ff671b;
+	color: #f7f7f7;
+}
+.all-tab .tab-content {
+	padding: 100px 0;
+}
+.tab-main {
+	display: flex;
+}
+.tab-drop,
+.contact-details {
+	width: calc(48% - 15px);
+	margin: 15px;
+}
+.drop-sub {
+	border: solid 1px #77777a;
+	max-width: 420px;
+	padding: 50px 40px;
+}
+.drop-sub h2 {
+	font-family: sansationbold;
+	font-size: 26px;
+	color: #111820;
+	line-height: 21px;
+	letter-spacing: -0.5px;
+	margin-bottom: 25px;
+}
+.drop-sub h3 {
+	font-family: sansationregular;
+	font-size: 26px;
+	color: #111820;
+	line-height: 21px;
+	letter-spacing: -0.5px;
+	margin-bottom: 35px;
+}
+.drop-sub select {
+	width: 100%;
+	padding: 10px;
+	border: 1px solid #8c8c8c;
+	border-radius: 4px;
+	font-family: sansationbold;
+	font-size: 20px;
+	color: #77777a;
+}
+.contact-details {
+	border-radius: 10px;
+	background: #f7f7f7;
+	box-shadow: 0px 12px 25px rgba(17,24,32,0.1);
+	padding: 50px;
+	text-align: center;
+}
+.contact-details h2 {
+	font-family: sansationbold;
+	font-size: 26px;
+	color: #111820;
+	line-height: 21px;
+	letter-spacing: -0.5px;
+	text-align: center;
+	padding-bottom: 20px;
+}
+.contact-details h3 {
+	font-family: Nunito,sans-serif;
+	font-size: 36px;
+	color: #ff671b;
+	line-height: 30px;
+	text-align: center;
+	padding-bottom: 30px;
+}
+.contact-details a img  {
+	margin-right: 15px;
+}
+.contact-details a {
+	font-family: Nunito,sans-serif;
+	font-size: 20px;
+	color: #77777a;
+	line-height: 1px;
+	text-align: center;
+}</style>
