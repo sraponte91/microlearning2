62ff24189052fbbe29b323d2ef27de53ede0ffa2
santy2214 <31638149+santy2214@users.noreply.github.com>
2021-05-11 14:32:35 -0500

    change in menu red de angeles 11052021-14:32

--- components/Header.vue
+++ components/Header.vue
@@ -111,7 +111,7 @@
                       >Inversión Social</b-dropdown-item
                     >
                     <b-dropdown-item to="/red-de-angeles"
-                      >Red De Ángeles</b-dropdown-item
+                      >Red de Ángeles</b-dropdown-item
                     >
                     <b-dropdown-item to="/voluntariado-fundacion-bolivar"
                       >Voluntariado Fundación Bolivar</b-dropdown-item
