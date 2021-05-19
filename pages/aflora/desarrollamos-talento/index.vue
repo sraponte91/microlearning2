595dad65d9d89ebd4ba8fd15837ed77ec185e6eb
santy2214 <31638149+santy2214@users.noreply.github.com>
2021-05-19 09:01:40 -0500

    niveles in aflora-desarrollamos talento 19052021-9:01

--- pages/aflora/desarrollamos-talento/index.vue
+++ pages/aflora/desarrollamos-talento/index.vue
@@ -7,13 +7,9 @@
           <b-breadcrumb-item to="/aflora">Aflora</b-breadcrumb-item>
           <b-breadcrumb-item active>Desarrollamos talento</b-breadcrumb-item>
         </b-breadcrumb>
-        <div
-          class="back-to-home position-relative d-flex align-items-center small-size"
-        >
+        <div class="back-to-home position-relative d-flex align-items-center small-size">
           <i class="ic-back" @click="goBack()"></i>
-          <a class="ff-sans-b" href="javascript:void(0)" @click="goBack()"
-            >Volver</a
-          >
+          <a class="ff-sans-b" href="javascript:void(0)" @click="goBack()">Volver</a>
         </div>
       </div>
     </div>
@@ -22,17 +18,11 @@
       <div class="container">
         <div class="sm-siz-container margin-0-auto">
           <div class="d-flex multiple-reapt align-items-start flex-wrap-991">
-            <h2
-              class="ff-sans-b ss d-block-991 d-none-n wow bounceInLeft"
-              data-wow-duration="2s"
-            >
+            <h2 class="ff-sans-b ss d-block-991 d-none-n wow bounceInLeft" data-wow-duration="2s">
               Desarrollamos <br />
               talento
             </h2>
-            <div
-              class="txt-points position-relative order-2-991 wow bounceInLeft"
-              data-wow-duration="2s"
-            >
+            <div class="txt-points position-relative order-2-991 wow bounceInLeft" data-wow-duration="2s">
               <h2 class="ff-sans-b d-none-991">
                 Desarrollamos <br />
                 talento
@@ -54,26 +44,15 @@
                 </p>
               </div>
             </div>
-            <div
-              class="img-points order-1-991 wow bounceInRight"
-              data-wow-duration="2s"
-            >
-              <img
-                src="/images/programa-aflora/3-desarrollamos-talento/desarrollamostalento_bloque1.jpg"
-                alt=""
-                class="object-fit-cover"
-              />
+            <div class="img-points order-1-991 wow bounceInRight" data-wow-duration="2s">
+              <img src="/images/programa-aflora/3-desarrollamos-talento/desarrollamostalento_bloque1.jpg" alt=""
+                class="object-fit-cover" />
             </div>
           </div>
-          <div
-            class="d-flex multiple-reapt align-items-start pp-lr flex-wrap-767"
-          >
+          <div class="d-flex multiple-reapt align-items-start pp-lr flex-wrap-767">
             <div class="oly-img-bx wow bounceInLeft" data-wow-duration="2s">
-              <img
-                src="/images/programa-aflora/3-desarrollamos-talento/desarrollamostalento_bloque2_destacado.jpg"
-                alt=""
-                class="object-fit-cover"
-              />
+              <img src="/images/programa-aflora/3-desarrollamos-talento/desarrollamostalento_bloque2_destacado.jpg"
+                alt="" class="object-fit-cover" />
             </div>
             <div class="oly-tx-bx wow bounceInRight" data-wow-duration="2s">
               <div class="inn-bx-bg ff-sans-b">
@@ -82,10 +61,7 @@
                   gratuita con prestigiosas universidades.
                 </p>
                 <div class="links-to-r text-right">
-                  <NuxtLink
-                    to="/aflora/desarrollamos-talento/ciclos-de-formacion"
-                    >Quiero conocer más ></NuxtLink
-                  >
+                  <NuxtLink to="/aflora/desarrollamos-talento/ciclos-de-formacion">Quiero conocer más ></NuxtLink>
                 </div>
               </div>
             </div>
@@ -112,6 +88,74 @@
 
                 Estas son las características de cada nivel: <br />
               </p>
+              <!--New design--->
+              <div class="four-boxes">
+                <div class="d-flex flex-wrap">
+                  <div class="one-box text-center position-relative">
+                    <div
+                      class="round-bx position-relative margin-0-auto d-flex align-items-center justify-content-center">
+                      <div
+                        class="numbr text-white ff-sans-b d-flex align-items-center justify-content-center position-absolute">
+                        1
+                      </div>
+                      <p class="ff-sans-sb">
+                        En esta sección se encontrará (i) una plataforma de e-learning; (ii) información y
+                        acceso para procesos de formación con aliados; (iii) información y acceso para
+                        procesos de formación abiertas de ingreso con correo electrónico; y (iv)
+                        facilidades para preparación de giras.
+                      </p>
+                    </div>
+                  </div>
+                  <div class="one-box text-center position-relative">
+                    <div
+                      class="round-bx position-relative margin-0-auto d-flex align-items-center justify-content-center">
+                      <div
+                        class="numbr text-white ff-sans-b d-flex align-items-center justify-content-center position-absolute">
+                        2
+                      </div>
+                      <p class="ff-sans-sb">
+                        Para acceder a la misma, El Portal cuenta con las habilitaciones necesarias para
+                        que el Usuario designe un usuario y contraseña. Los datos del Usuario que se
+                        recolecten con ocasión de la anterior designación se tratarán de acuerdo con lo
+                        indicado en la sección AUTORIZACIONES QUE USTED OTORGA de los Términos
+                        Generales
+                      </p>
+                    </div>
+                  </div>
+                  <div class="one-box text-center position-relative">
+                    <div
+                      class="round-bx position-relative margin-0-auto d-flex align-items-center justify-content-center">
+                      <div
+                        class="numbr text-white ff-sans-b d-flex align-items-center justify-content-center position-absolute">
+                        3
+                      </div>
+                      <p class="ff-sans-sb">
+                        Los procesos de formación serán evaluados por el Usuario
+                      </p>
+                    </div>
+                  </div>
+                  <div class="one-box text-center position-relative">
+                    <div
+                      class="round-bx position-relative margin-0-auto d-flex align-items-center justify-content-center">
+                      <div
+                        class="numbr text-white ff-sans-b d-flex align-items-center justify-content-center position-absolute">
+                        4
+                      </div>
+                      <p class="ff-sans-sb">
+                        En relación con los procesos de formación con aliados, FBD hace claridad que la
+                        información contenido en El Portal se publica con propósitos informativos, por lo
+                        que FBD no respalda los proveedores que se referencien en esta sección. En
+                        consecuencia, FBD, la FUNDACIÓN CULTIVA EL ARTE Y LA CULTURA, ni el
+                        GRUPO EMPRESARIAL BOLÍVAR serán responsables, individual o
+                        conjuntamente, ante los usuarios de dichos programas o de los servicios
+                        prestados por estos aliados
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!--Close New design--->
+
             </div>
             <!--<div class="btn-g">
               <a :href="$afloraData.afloraDesarrollamosTalento.nivelesLink" 
@@ -128,11 +172,8 @@
         <div class="sm-siz-container margin-0-auto">
           <div class="position-relative for-mt-mins">
             <div class="im-prt">
-              <img
-                src="/images/programa-aflora/3-desarrollamos-talento/escuela_de_directores_desktop.jpg"
-                alt=""
-                class="object-fit-cover"
-              />
+              <img src="/images/programa-aflora/3-desarrollamos-talento/escuela_de_directores_desktop.jpg" alt=""
+                class="object-fit-cover" />
             </div>
             <div class="txt-sec margin-0-auto">
               <div class="text-center">
@@ -156,11 +197,8 @@
                 </p>
               </div>
               <div class="btn-g text-center">
-                <a
-                  :href="$afloraData.afloraDesarrollamosTalento.escuelaLink"
-                  class="btn-know-leave ff-sans-b"
-                  >Conocer Niveles</a
-                >
+                <a :href="$afloraData.afloraDesarrollamosTalento.escuelaLink" class="btn-know-leave ff-sans-b">Conocer
+                  Niveles</a>
               </div>
             </div>
           </div>
@@ -170,51 +208,47 @@
   </main>
 </template>
 <script>
-export default {
-  head() {
-    return {
-      title:
-        this.$afloraData.afloraDesarrollamosTalento.meta.title +
-        ' - ' +
-        this.$constants.home.meta.title,
-      meta: [
-        {
-          hid: 'title',
-          name: 'title',
-          content:
-            this.$afloraData.afloraDesarrollamosTalento.meta.metaTitle +
-            ' - ' +
-            this.$constants.home.meta.title,
-        },
-        {
-          hid: 'description',
-          name: 'description',
-          content: this.$afloraData.afloraDesarrollamosTalento.meta
-            .metaDescription,
-        },
-        {
-          hid: 'og:title',
-          name: 'og:title',
-          content:
-            this.$afloraData.afloraDesarrollamosTalento.meta.ogTitle +
-            ' - ' +
-            this.$constants.home.meta.title,
-        },
-        {
-          hid: 'og:description',
-          name: 'og:description',
-          content: this.$afloraData.afloraDesarrollamosTalento.meta
-            .ogDescription,
-        },
-        {
-          hid: 'og:image',
-          name: 'og:image',
-          content:
-            this.$constants.baseURL +
-            this.$afloraData.afloraDesarrollamosTalento.meta.ogImage,
-        },
-      ],
-    }
-  },
-}
+  export default {
+    head() {
+      return {
+        title: this.$afloraData.afloraDesarrollamosTalento.meta.title +
+          ' - ' +
+          this.$constants.home.meta.title,
+        meta: [{
+            hid: 'title',
+            name: 'title',
+            content: this.$afloraData.afloraDesarrollamosTalento.meta.metaTitle +
+              ' - ' +
+              this.$constants.home.meta.title,
+          },
+          {
+            hid: 'description',
+            name: 'description',
+            content: this.$afloraData.afloraDesarrollamosTalento.meta
+              .metaDescription,
+          },
+          {
+            hid: 'og:title',
+            name: 'og:title',
+            content: this.$afloraData.afloraDesarrollamosTalento.meta.ogTitle +
+              ' - ' +
+              this.$constants.home.meta.title,
+          },
+          {
+            hid: 'og:description',
+            name: 'og:description',
+            content: this.$afloraData.afloraDesarrollamosTalento.meta
+              .ogDescription,
+          },
+          {
+            hid: 'og:image',
+            name: 'og:image',
+            content: this.$constants.baseURL +
+              this.$afloraData.afloraDesarrollamosTalento.meta.ogImage,
+          },
+        ],
+      }
+    },
+  }
+
 </script>
